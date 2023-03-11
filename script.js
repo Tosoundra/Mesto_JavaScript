1cf060ccfcc7e67bcfaed6be5fe8769b633aae73
Antony Kowalski <beheaded_96@mail.ru>
2023-03-11 15:55:34 +0300

    feat:adaptive

--- script.js
+++ script.js
@@ -3,90 +3,60 @@ let closeButton = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup_closed');
 let submit = document.querySelector('.popup__submit');
 let sectionElement = document.querySelector('.elements');
-let likeButtons = sectionElement.querySelectorAll('.places__like');
+let likeButton = sectionElement.querySelectorAll('.places__like');
 
 let form = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__name');
 let occupationInput = document.querySelector('.popup__occupation');
 let profileName = document.querySelector('.profile__title');
 let profileOccupation = document.querySelector('.profile__subtitle');
+let prikol = document.querySelector('.video');
 
-
-
+profileName.textContent = 'Жак-Ив Кусто';
+profileOccupation.textContent = 'Исследователь океана';
 
 function handleFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    profileName.textContent = nameInput.value;
-    profileOccupation.textContent = occupationInput.value;
-    popup.classList.remove('popup');
-};
+  profileName.textContent = nameInput.value;
+  profileOccupation.textContent = occupationInput.value;
+  popup.classList.remove('popup');
+  prikol.style.display = 'block';
+  prikol.removeAttribute('muted');
+}
 
 form.addEventListener('submit', handleFormSubmit);
 
-
-
 editButton.addEventListener('click', function () {
-    popup.classList.add('popup');
-}
-);
+  popup.classList.add('popup');
+});
 
 closeButton.addEventListener('click', function () {
-    popup.classList.remove('popup');
+  popup.classList.remove('popup');
 });
 
-likeButtons[0].addEventListener('click', function () {
-    if (likeButtons[0].attributes[1].value === 'images/like-icon.svg')
-    likeButtons[0].setAttribute('src', 'images/like-active.svg');
+function likeButtonClick() {
+  if (this.attributes[1].value === 'images/like-icon.svg') {
+    this.setAttribute('src', 'images/like-active.svg');
+  } else {
+    this.setAttribute('src', 'images/like-icon.svg');
+  }
+}
 
-    else {
-        likeButtons[0].setAttribute('src', 'images/like-icon.svg');
-    }
+likeButton.forEach(button => {
+  button.addEventListener('click', likeButtonClick);
 });
 
-likeButtons[1].addEventListener('click', function () {
-    if (likeButtons[1].attributes[1].value === 'images/like-icon.svg')
-    likeButtons[1].setAttribute('src', 'images/like-active.svg');
-
-    else {
-        likeButtons[1].setAttribute('src', 'images/like-icon.svg');
-    }
-});
-
-
-likeButtons[2].addEventListener('click', function () {
-    if (likeButtons[2].attributes[1].value === 'images/like-icon.svg')
-    likeButtons[2].setAttribute('src', 'images/like-active.svg');
-
-    else {
-        likeButtons[2].setAttribute('src', 'images/like-icon.svg');
-    }
-});
-
-likeButtons[3].addEventListener('click', function () {
-    if (likeButtons[3].attributes[1].value === 'images/like-icon.svg')
-    likeButtons[3].setAttribute('src', 'images/like-active.svg');
-
-    else {
-        likeButtons[3].setAttribute('src', 'images/like-icon.svg');
-    }
-});
-
-likeButtons[4].addEventListener('click', function () {
-    if (likeButtons[4].attributes[1].value === 'images/like-icon.svg')
-    likeButtons[4].setAttribute('src', 'images/like-active.svg');
-
-    else {
-        likeButtons[4].setAttribute('src', 'images/like-icon.svg');
-    }
-});
-
-likeButtons[5].addEventListener('click', function () {
-    if (likeButtons[5].attributes[1].value === 'images/like-icon.svg')
-    likeButtons[5].setAttribute('src', 'images/like-active.svg');
-
-    else {
-        likeButtons[5].setAttribute('src', 'images/like-icon.svg');
-    }
-});
-
+//  смена цвета кнопки при нажатии (другой принцип)
+// let iconButton = document.querySelectorAll('.svg-fill');
+// function action() {
+//     if(this.attributes[1].value === 'white') {
+//          this.setAttribute('fill', 'black');
+//     }
+//     else {
+//         this.setAttribute('fill', 'white')
+//     }
+// };
+// iconButton.forEach(button=> {
+//     button.addEventListener('click', action);
+// });
